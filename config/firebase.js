2be98b292674fa64c38bbdd34fe2e81edec29cde
Sreejith <sreejithsp711@gmail.com>
2024-02-17 20:22:31 +0530

    fix:env changes

--- config/firebase.js
+++ config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.API_ID,
+  apiKey: "AIzaSyC7wHNCq3ceK-yvlfb3EJ1V3bgqWln6qkw",
+  authDomain: "growthcx-todo.firebaseapp.com",
+  projectId: "growthcx-todo",
+  storageBucket: "growthcx-todo.appspot.com",
+  messagingSenderId: "613170789069",
+  appId: "1:613170789069:web:bac50aa2f06ade16ffc784",
 };
 
 initializeApp(firebaseConfig);
