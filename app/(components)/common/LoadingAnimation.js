22fc2511e4031476d3b1bc8fcb34e336ff43b683
Sreejith <sreejithsp711@gmail.com>
2024-02-18 09:27:22 +0530

    feat:changed the loading design and color

--- app/(components)/common/LoadingAnimation.js
+++ app/(components)/common/LoadingAnimation.js
@@ -5,9 +5,9 @@ const LoadingAnimation = () => {
   return (
     <div className="flex space-x-2 justify-center items-center bg-white h-screen ">
       <span className="sr-only">Loading...</span>
-      <div className="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-      <div className="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-      <div className="h-8 w-8 bg-black rounded-full animate-bounce"></div>
+      <div className="h-6 w-6 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+      <div className="h-6 w-6 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+      <div className="h-6 w-6 bg-blue-500 rounded-full animate-bounce"></div>
     </div>
   );
 };
